ffcd87266ce7914b3fe55302bb7e600c499fc911
Kevin Jordan <k.dean.jordan@gmail.com>
2021-01-25 09:14:52 -0800

    FEAT: added amplify and cognito

--- front/src/main.js
+++ front/src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+import aws_exports from './aws-exports';
+
 
 Vue.config.productionTip = false
+
+Amplify.configure(aws_exports)
+Vue.use(router)
 
 new Vue({
   router,
